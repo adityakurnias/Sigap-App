17ba55bdf36c028aee44d699abf546ba6d007ac3
adityakurnias <adityakurniasaputra903@gmail.com>
2026-01-26 21:41:15 +0700

    feat: auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+});
+
+// Jalur Khusus Member (Sudah Login)
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    // Dashboard Admin
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    // Dashboard Warga (Sementara)
+    Route::get('/warga/dashboard', function () {
+        return "Halo Warga! Ini halaman kamu.";
+    })->name('user.dashboard');
 });
